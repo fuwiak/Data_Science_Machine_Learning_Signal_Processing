b96118de11e6a1ab31d29b8afac1228c4b71e834
fuwiak <poczta130@gmail.com>
2018-06-15 21:26:27 +0200

    plot

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: pawel
+"""
+
+<<<<<<< HEAD
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+
+np.random.seed(0)
+X, y = datasets.make_moons(100, noise=0.10)
+plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+=======
+
+>>>>>>> f5c9079325fe9ca76032896d6c3b8ef695242ee3
