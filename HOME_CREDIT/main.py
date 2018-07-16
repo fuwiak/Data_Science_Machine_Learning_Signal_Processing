791aa83f9b9d70dc33c7b824512c1b51b8ebebac
fuwiak <poczta130@gmail.com>
2018-07-16 14:11:53 +0200

    enc

--- HOME_CREDIT/main.py
+++ HOME_CREDIT/main.py
@@ -1,7 +1,24 @@
 import pandas as pd
 import numpy as np
 import matplotlib as plt
+from contextlib import contextmanager
+
+@contextmanager
+def timer(title):
+    t0 = time.time()
+    yield
+    print("{} - done in {:.0f}s".format(title, time.time() - t0))
+
 
 dataset=pd.read_csv("../home_credit_source/application_train.csv")
 
 #description_set = data.describe()
+
+def one_hot_encoder(df, nan_as_category = True):
+    original_columns = list(df.columns)
+    categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
+    df = pd.get_dummies(df, columns= categorical_columns, dummy_na= nan_as_category)
+    new_columns = [c for c in df.columns if c not in original_columns]
+    return df, new_columns
+
+
