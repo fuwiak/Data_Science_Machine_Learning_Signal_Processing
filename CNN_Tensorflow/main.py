dd3832f2648a6be26fede4c0ed0bee224c6e7af5
fuwiak <poczta130@gmail.com>
2018-07-16 15:34:33 +0200

    lib

--- CNN_Tensorflow/main.py
+++ CNN_Tensorflow/main.py
@@ -1,1 +1,8 @@
-import numpy as np
+import numpy as np 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+import tensorflow as tf
+
+import matplotlib.pyplot as plt, matplotlib.image as mpimg
+import matplotlib.cm as cm
