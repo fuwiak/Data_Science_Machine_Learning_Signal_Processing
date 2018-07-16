cbf7308f8f05538bfc038eb9c536650dfb4f7650
fuwiak <poczta130@gmail.com>
2018-07-16 17:35:53 +0200

    nlp

--- CNN_Tensorflow/main.py
+++ CNN_Tensorflow/main.py
@@ -6,3 +6,7 @@ import tensorflow as tf
 
 import matplotlib.pyplot as plt, matplotlib.image as mpimg
 import matplotlib.cm as cm
+
+labeled_imag = pd.read_csv('train.csv')
+from sklearn.preprocessing import LabelBinarizer
+encoder = LabelBinarizer()
