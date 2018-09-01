1fd0c3560729f63014d7f043af62b7e6c41370f0
fuwiak <poczta130@gmail.com>
2018-09-01 12:51:47 +0200

    clean

--- pand.py
+++ pand.py
@@ -30,3 +30,14 @@ df.max() # Returns the highest value in each column
 df.min() # Returns the lowest value in each column
 df.median() # Returns the median of each column
 df.std() # Returns the standard deviation of each column
+
+# Selecting
+
+df[col] # Returns column with label col as Series
+df[[col1, col2]] # Returns Columns as a new DataFrame
+s.iloc[0] # Selection by position (selects first element)
+s.loc[0] # Selection by index (selects element at index 0)
+df.iloc[0,:] # First row
+df.iloc[0,0] # First element of first column
+
+# Data Cleaning
