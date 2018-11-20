f4fd12d8e427d42334ce6ab97a3e3746c0592c5d
Pawel <poczta130@gmail.com>
2018-11-21 00:43:26 +0100

    Update learning_curve.py

--- ML_for_tutoring/learning_curve.py
+++ ML_for_tutoring/learning_curve.py
@@ -16,14 +16,3 @@ def plot_learning_curve(estimator):
 	plt.show()
 clf = svm.SVC(kernel='rbf',C=C_grid,gamma=gamma_grid)
 #plot_learning_curve(clf)
-
-def apply_pca(train_images,test_images,components):
-	pca = PCA(n_components=components)
-	train_images = pca.fit_transform(train_images)
-	test_images = pca.transform(test_images)
-	print train_images.shape
-	print test_images.shape
-	return train_images,test_images
-
-train_images,test_images = apply_pca(train_images,test_images,0.7)
-clf.fit(train_images, train_labels)
