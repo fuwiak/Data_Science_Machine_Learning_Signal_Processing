a2cbe186f9aec09012aa22c11959f2328b84c216
Pawel <poczta130@gmail.com>
2019-01-26 18:36:47 +0100

    Update cheatsheet.py

--- cheatsheet.py
+++ cheatsheet.py
@@ -1,2 +1,5 @@
 #concatenate train and test set, to keep their indexes just dont use the ignore_index keyword
 dataset =pd.concat([train_df, test_df], axis=0)
+
+#importance of columns
+importances = list(zip(model.feature_importances_, df.columns))
