9e1fb82349e3b5e04ac776b697bacfe223caecc9
Pawel <poczta130@gmail.com>
2019-02-21 12:11:45 +0100

    Update cheatsheet.py

--- cheatsheet.py
+++ cheatsheet.py
@@ -1,3 +1,7 @@
+# Replace using median 
+median = df['NUM_BEDROOMS'].median()
+df['NUM_BEDROOMS'].fillna(median, inplace=True)
+
 #concatenate train and test set, to keep their indexes just dont use the ignore_index keyword
 dataset =pd.concat([train_df, test_df], axis=0)
 
